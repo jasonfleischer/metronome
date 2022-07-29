30315b2f261f742549f095214dc469e79fd41613
Jason Fleischer <jason_fleischer@hotmail.ca>
2022-07-28 20:34:43 -0400

    update

--- service_worker.js
+++ service_worker.js
@@ -1,35 +1,9 @@
-let CACHE_NAME = 'v4.1';
+let CACHE_NAME = 'v5';
 const CACHE = [
         '/metronome/index.html',
         '/metronome/fonts/SF_Pro_Display_Thin.woff2',
         '/metronome/css/bundle.css',
-        '/metronome/js/bundle.js',
-        /*'/metronome/css/root.css',
-        '/metronome/css/main.css',
-        '/metronome/css/select.css',
-        '/metronome/css/slider.css',
-        '/metronome/css/switch.css',
-        '/metronome/css/button.css',
-        '/metronome/css/alert.css',
-        '/metronome/js/midi/webAudioFontPlayer.js',
-        '/metronome/js/log.js',
-        '/metronome/js/midi/midi_sounds.js',
-        '/metronome/js/midi/click_midi_controller.js',
-        '/metronome/js/midi/drum_midi_controller.js',
-        '/metronome/js/model.js',
-        '/metronome/js/translations/generated.js',
-        '/metronome/js/translations/translations.js',
-        '/metronome/js/prototypes.js',
-        '/metronome/js/storage.js',
-        '/metronome/js/range_control.js',
-        '/metronome/js/time_view.js',
-        '/metronome/js/audio_controller.js',
-        '/metronome/js/tap_controller.js',
-        '/metronome/js/keyboard_shortcuts.js',
-        '/metronome/js/alert.js',
-        '/metronome/js/information.js',
-        '/metronome/js/main.js',
-        '/metronome/js/install.js'*/
+        '/metronome/js/bundle.js'
       ];
 	
 self.addEventListener('install', function(event) {
